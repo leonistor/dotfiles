f3633a3c6e1e752aa89b8ec50262b7c500b6898d
Leo Nistor <leo.nistor@gmail.com>
2020-11-07 22:04:27 +0200

    emacs projectile search-path

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -59,3 +59,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; projectile
+(setq projectile-project-search-path '("~/learn" "~/exercism" "~/work"))
