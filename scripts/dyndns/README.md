eaa0f01ad94bf2039f1135eb9a55ae8e44932a60
Leo Nistor <leo.nistor@gmail.com>
2022-08-16 08:49:54 +0300

    add dyndns scripts

---
+++ scripts/dyndns/README.md
@@ -0,0 +1,9 @@
+# DynDNS tools
+
+# my script, using curl
+inspiration script: https://gist.github.com/beancurd1/a55e299dc125810cba3561fda01642a7
+doc: https://www.dynu.com/DynamicDNS/IPUpdateClient/cURL
+
+# other methods
+ddclient: https://www.dynu.com/DynamicDNS/IPUpdateClient/DDClient
+dynu: https://www.dynu.com/DynamicDNS/IPUpdateClient/Linux
---
+++ scripts/dyndns/dynu_update_ip.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Update IP for dyndns hosts at dynu.com
+
+DYNU_USERNAME="leonistor"
+DYNU_PASS_HASH="db8e699794924ff2f6cb21702c3129cc"
+DYNU_HOSTNAME="giantchicken.giize.com"
+
+
+EXTERNAL_IP=$(curl -s "https://api.ipify.org")
+CURRENT_IP=$(dig +short @ns1.dynu.com $DYNU_HOSTNAME)
+
+DATETIME=$(date +"%Y-%m-%d:%H:%M")
+
+# CURL_UPDATE="https://api.dynu.com/nic/update?username=$DYNU_USERNAME&password=$DYNU_PASS_HASH&hostname=$DYNU_HOSTNAME&myip=$EXTERNAL_IP"
+CURL_UPDATE="https://XXXapi.dynu.com/nic/update?username=$DYNU_USERNAME&password=$DYNU_PASS_HASH&hostname=$DYNU_HOSTNAME&myip=$EXTERNAL_IP"
+
+LOG_NOOP="[$DATETIME] NOOP"
+LOG_UPDATE="[$DATETIME] UPDATE"
+LOG_DETAILS="CURRENT_IP=$CURRENT_IP EXTERNAL_IP=$EXTERNAL_IP"
+
+if [ "$EXTERNAL_IP" = "$CURRENT_IP" ]
+then
+  printf "$LOG_NOOP $LOG_DETAILS\n"
+else
+  printf "$LOG_UPDATE $LOG_DETAILS\n"
+  curl $CURL_UPDATE
+fi
